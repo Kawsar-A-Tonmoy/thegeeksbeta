c36f34d9bc767f906e63d45937360eacca753dc4
Kawsar-A-Tonmoy <kawsar.4hm3d@gmail.com>
2025-08-21 13:54:28 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,19 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const firebaseConfig = {
-  apiKey: "${{ secrets.API_KEY }}",
-  authDomain: "geekshopbd.firebaseapp.com",
-  projectId: "geekshopbd",
-  storageBucket: "geekshopbd.firebasestorage.app",
-  messagingSenderId: "754381329378",
-  appId: "1:754381329378:web:09c3fe0d507a1749de5fbb"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
 
 export const BKASH_NUMBER = '01960788862'; // Replace with your Bkash number
 export const COD_NUMBER = '01960788862'; // Replace with your COD contact number
 
 export const DELIVERY_FEE = 70; // Default delivery fee
+
