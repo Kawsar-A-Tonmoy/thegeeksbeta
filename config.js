0485dede00218fa0694fbefec77d23406b1a0875
Kawsar-A-Tonmoy <kawsar.4hm3d@gmail.com>
2025-10-20 15:27:59 +0600

    Change Firebase config for new project settings
    Updated Firebase configuration details.

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAANaVQR8MbppZmhBkmZ2ptlzZEbwNMgAQ",
-  authDomain: "thebeta-bca97.firebaseapp.com",
-  projectId: "thebeta-bca97",
-  storageBucket: "thebeta-bca97.firebasestorage.app",
-  messagingSenderId: "430455154137",
-  appId: "1:430455154137:web:f305f234ae17a0017aa5c8",
-  measurementId: "G-JJEXR06M39"
+ export const firebaseConfig = {
+  apiKey: "AIzaSyBYzuaMcH_fH43e9qgdntIH7ez29wVpzaU",
+  authDomain: "thegeeks-1b8aa.firebaseapp.com",
+  projectId: "thegeeks-1b8aa",
+  storageBucket: "thegeeks-1b8aa.firebasestorage.app",
+  messagingSenderId: "662440200085",
+  appId: "1:662440200085:web:f6d8ecfdc1f4b6d23a189c",
+  measurementId: "G-ZSCC8Z80SB"
 };
 
 
@@ -13,6 +13,7 @@ export const BKASH_NUMBER = '01960788862 (Personal)'; // Replace with your Bkash
 export const COD_NUMBER = '01960788862 (Personal)'; // Replace with your COD contact number
 
 export const DELIVERY_FEE = 70; // Default delivery fee
+
 
 
 
